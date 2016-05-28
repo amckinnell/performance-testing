273db018897f72178639a475fee8a2aa510ee22c
Alistair McKinnell <alistair.mckinnell@gmail.com>
2016-05-28 12:09:07 -0400

    Add the ResultsDirectory class.

---
+++ lib/results_directory.rb
@@ -0,0 +1,26 @@
+#
+# Knows how to create a timestamped and tagged directory.
+#
+class ResultsDirectory
+  DEFAULT_RESULTS_DIR = "./profile_results"
+
+  def initialize(results_dir: DEFAULT_RESULTS_DIR)
+    @results_dir = results_dir
+  end
+
+  def create(tag:)
+    results_directory_name(tag).tap do |name|
+      FileUtils.mkdir_p(name)
+    end
+  end
+
+  private
+
+  def results_directory_name(tag)
+    File.join(@results_dir, "#{timestamp}-#{tag}")
+  end
+
+  def timestamp
+    Time.now.strftime("%Y-%m-%dT%H%M%S")
+  end
+end
